e8a0e6bfa0a3057a1c27ecfbee5f24ca7ec3cca8
Test Kbs <ritikkumawat140@gmail.com>
2024-07-26 18:42:31 +0530

    intial project added

---
+++ lib/main.dart
@@ -0,0 +1,233 @@
+import 'package:agora_uikit/agora_uikit.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+const appId = "bbdcadb6d17d422aa78f1f2c5847a84b";
+// const token = "<-- Insert Token -->";
+const channel = "Android";
+const appCertificate = "bf4f5365e777428184bf39bb2a4f7a9a";
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var client=AgoraClient(agoraConnectionData: AgoraConnectionData(appId:appId , channelName: 'testing'),enabledPermission: [Permission.camera,Permission.microphone]);
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    client.initialize();
+  }
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body:Stack(
+        children: [
+          AgoraVideoViewer(client: client),
+          AgoraVideoButtons(client: client),
+        ],
+      )
+     );
+  }
+}
+
+
+//
+// import 'dart:async';
+//
+// import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+// import 'package:agora_token_service/agora_token_service.dart';
+// import 'package:flutter/material.dart';
+// import 'package:permission_handler/permission_handler.dart';
+//
+
+// void main() => runApp(const MaterialApp(home: MyApp()));
+//
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   int? _remoteUid;
+//   bool _localUserJoined = false;
+//   late RtcEngine _engine;
+//
+//   late var token = '';
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     initAgora();
+//     token=  RtcTokenBuilder.build(
+//       appId: appId,
+//       appCertificate: appCertificate,
+//       channelName: "Agora",
+//       uid: "0",
+//       role: RtcRole.subscriber,
+//       expireTimestamp: 3600,
+//     );
+//   }
+//
+//   Future<void> initAgora() async {
+//     // retrieve permissions
+//     await [Permission.microphone, Permission.camera].request();
+//
+//     //create the engine
+//     _engine = createAgoraRtcEngine();
+//     await _engine.initialize(const RtcEngineContext(
+//       appId: appId,
+//       channelProfile: ChannelProfileType.channelProfileLiveBroadcasting,
+//     ));
+//
+//     _engine.registerEventHandler(
+//       RtcEngineEventHandler(
+//         onJoinChannelSuccess: (RtcConnection connection, int elapsed) {
+//           debugPrint("local user ${connection.localUid} joined");
+//           setState(() {
+//             _localUserJoined = true;
+//           });
+//         },
+//         onUserJoined: (RtcConnection connection, int remoteUid, int elapsed) {
+//           debugPrint("remote user $remoteUid joined");
+//           setState(() {
+//             _remoteUid = remoteUid;
+//           });
+//         },
+//         onUserOffline: (RtcConnection connection, int remoteUid,
+//             UserOfflineReasonType reason) {
+//           debugPrint("remote user $remoteUid left channel");
+//           setState(() {
+//             _remoteUid = null;
+//           });
+//         },
+//         onTokenPrivilegeWillExpire: (RtcConnection connection, String token) {
+//           debugPrint(
+//               '[onTokenPrivilegeWillExpire] connection: ${connection.toJson()}, token: $token');
+//         },
+//       ),
+//     );
+//     await _engine.setClientRole(role: ClientRoleType.clientRoleBroadcaster);
+//     await _engine.enableVideo();
+//     await _engine.startPreview();
+//
+//     await _engine.joinChannel(
+//       token: token,
+//       channelId: channel,
+//       uid: 0,
+//       options: const ChannelMediaOptions(),
+//     );
+//   }
+//
+//   @override
+//   void dispose() {
+//     super.dispose();
+//
+//     _dispose();
+//   }
+//
+//   Future<void> _dispose() async {
+//     await _engine.leaveChannel();
+//     await _engine.release();
+//   }
+//
+//   // Create UI with local view and remote view
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Agora Video Call'),
+//       ),
+//       body: Stack(
+//         children: [
+//           Center(
+//             child: _remoteVideo(),
+//           ),
+//           Align(
+//             alignment: Alignment.topLeft,
+//             child: SizedBox(
+//               width: 100,
+//               height: 150,
+//               child: Center(
+//                 child: _localUserJoined
+//                     ? AgoraVideoView(
+//                   controller: VideoViewController(
+//                     rtcEngine: _engine,
+//                     canvas: const VideoCanvas(uid: 0),
+//                   ),
+//                 )
+//                     : const CircularProgressIndicator(),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+//
+//   // Display remote user's video
+//   Widget _remoteVideo() {
+//     if (_remoteUid != null) {
+//       return AgoraVideoView(
+//         controller: VideoViewController.remote(
+//           rtcEngine: _engine,
+//           canvas: VideoCanvas(uid: _remoteUid),
+//           connection: const RtcConnection(channelId: channel),
+//         ),
+//       );
+//     } else {
+//       return const Text(
+//         'Please wait for remote user to join',
+//         textAlign: TextAlign.center,
+//       );
+//     }
+//   }
+// }
